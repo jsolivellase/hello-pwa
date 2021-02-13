236bcbfb9a1cebc78071debaddd9c7b171a3b97e
Jaume Solivellas Estrany <jsolivellase@hotmail.com>
2021-02-13 18:04:58 +0100

    https://developer.okta.com/blog/2019/05/30/vue-pwa-typescript

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,17 @@ import Vue from 'vue'
 import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+import VueMaterial from 'vue-material'
+import 'vue-material/dist/vue-material.min.css'
+import 'vue-material/dist/theme/default-dark.css'
+import { cacheAdapterEnhancer } from 'axios-extensions';
+
+Vue.use(VueMaterial);
+Vue.use(VueAxios, axios.create({
+  adapter: cacheAdapterEnhancer(axios.defaults.adapter as any)
+}));
 
 Vue.config.productionTip = false
 
